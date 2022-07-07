fa2f356f0816d1ed8f63ba784dc76f7a1d401d80
sygu <sygu@uw.edu>
2022-07-06 21:38:18 -0700

    First Commit

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passpoerLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+UserSchema.plugin(passpoerLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
